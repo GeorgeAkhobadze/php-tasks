d3b96731b705a9e54a28b4bf1591002b9b37bfe3
Giorgi Akhobadze <axobadzegiorgi5@gmail.com>
2024-05-30 17:00:53 +0400

    regex round change

--- averageCalculator/calculator.php
+++ averageCalculator/calculator.php
@@ -3,9 +3,9 @@
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $numbers = $_POST['numbers'];
     $result = '';
-    if(preg_match("/^\d+(,\d+)*$/", $numbers)) {
+    if(preg_match("/^\d+(?:\.\d+)?(?:,\d+(?:\.\d+)?)*$/", $numbers)) {
         $result = explode(',', $numbers);
-        $average = array_sum($result) / count($result);
+        $average = round(array_sum($result) / count($result), 2);;
     } else {
         $error = 'Invalid Input, example: 1,2,3';
     }
